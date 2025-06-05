24b511bd29842b8c2d9eeddd2bee88cdbea8df95
Nupur Sharma <write2nupu@gmail.com>
2025-06-05 10:58:02 +0530

    File created

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Nupur on 05/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
